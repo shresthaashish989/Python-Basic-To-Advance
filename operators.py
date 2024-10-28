320c7be1089123fec9d12574154582d9046e679f
Ashish Shrestha <shresthaashish989@gmail.com>
2024-10-28 10:40:54 +0545

    abc

---
+++ operators.py
@@ -0,0 +1,118 @@
+# Arthemetic operator
+# relational operator
+# logical operator
+# assignment operator
+# bitwise operator
+#ternary operator
+#shift operator
+#special operators
+#equality operators
+#membership operators
+#identity operators
+
+#Arthematic operators add subtract division multiply modulas
+# in this arthematic operator +(concat) and *(string repetation) also used in string
+
+a=10
+b=2
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Division:",a/b)
+print("Multiplication:",a*b)
+print("Modulas:",a%b)
+print(a//b)
+print(a**b) # power operator
+print("Concatination:",a+b) # string
+print("String repetation:",a*"a") # string
+# relational operator
+# relational operator are used to compare the value of two variables
+# relational operator are ==, !=, >, <, >=, <=
+a=10
+b=20
+print("Equal to:",a==b)
+print("Not Equal to:",a!=b)
+print("Greater than:",a>b)
+print("Less than:",a<b)
+print("Greater than or equal to:",a>=b)
+print("Less than or equal to:",a<=b)
+# logical operator
+# logical operator are used to combine two or more conditions
+# logical operator are and, or, not
+a=10
+b=20
+print("And operator:",a>10 and b<20)
+print("Or operator:",a>10 or b<20)
+print("Not operator:",not(a>10))
+# assignment operator
+# assignment operator are used to assign the value to the variable
+# assignment operator are =, +=, -=, *=, /=, %=, **=, //=
+a = 10
+b = 20
+
+a += b
+print("Addition assignment:", a)
+
+a -= b
+print("Subtraction assignment:", a)
+
+a *= b
+print("Multiplication assignment:", a)
+
+a /= b
+print("Division assignment:", a)
+
+a %= b
+print("Modulus assignment:", a)
+
+a **= b
+print("Power assignment:", a)
+
+a //= b
+print("Floor division assignment:", a)
+
+
+# membership operator
+# membership operator are used to check the value is present in the sequence or not
+# membership operator are in, not in
+a=[1,2,3,4,5]
+b=3
+print("Not in operator:",5 not in a)
+print("In operator:",b in a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
