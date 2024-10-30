abc210350959c0c1b92ae6f4df983692c57651e7
Ashish Shrestha <shresthaashish989@gmail.com>
2024-10-30 16:37:03 +0545

    abc

---
+++ convert_number_to_words.py.py
@@ -0,0 +1,16 @@
+words_upto_19=["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+ "seventeen", "eighteen", "nineteen"]
+words_upto_tens=["", "", "twenty", "thirty", "forty", "fifty","sixty", "seventy", "eighty", "ninety"]
+
+n=int(input("enter the number:"))
+if n==0:
+    print("zero")
+elif n<=19:
+    print(words_upto_19[n])
+elif n<=99:
+    print(words_upto_tens[n//10]+words_upto_19[n%10])
+else:
+    print("please enter number between 0 to 99")
+
+
+
---
+++ pricesplit.py
@@ -0,0 +1,4 @@
+price=float(input("enter the total price"))
+person=float(input("ente the total person"))
+individual_price=price/person
+print("the individual price is",individual_price)
